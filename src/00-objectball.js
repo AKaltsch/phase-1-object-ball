29a53bc2b01da3bda197297f51d726beae41eed9
alec <kaltschmidalec@gmail.com>
2021-07-22 08:09:00 -0700

    commit tree 61198b4b961830888e2510326e82ed73d6be88b7

---
+++ src/00-objectball.js
@@ -0,0 +1,142 @@
+const gameObject = { 
+    home: {}
+}
+
+const player1 = {
+    playerName: "Alan Anderson",
+    Number : 0,
+    Shoe : 16,
+    Points : 22,
+    Rebounds : 12,
+    Assists : 12,
+    Steals : 3,
+    Blocks : 1,
+    slamDunks : 1, 
+}
+
+const player2 = {
+    playerName: "Reggie Evans",
+    Number : 30,
+    Shoe : 14,
+    Points : 12,
+    Rebounds : 12,
+    Assists : 12,
+    Steals : 12,
+    Blocks : 12,
+    slamDunks : 7, 
+}
+
+const player3 = {
+    playerName: "Brook Lopez",
+    Number : 11,
+    Shoe : 17,
+    Points : 17,
+    Rebounds : 19,
+    Assists : 10,
+    Steals : 3,
+    Blocks : 1,
+    slamDunks : 15, 
+}
+
+const player4 = {
+    playerName: "Mason Plumlee",
+    Number : 1,
+    Shoe : 19,
+    Points : 26,
+    Rebounds : 12,
+    Assists : 6,
+    Steals : 3,
+    Blocks : 8,
+    slamDunks : 5, 
+}
+
+const player5 = {
+    playerName: "Jason Terry",
+    Number : 31,
+    Shoe : 15,
+    Points : 19,
+    Rebounds : 2,
+    Assists : 2,
+    Steals : 4,
+    Blocks : 11,
+    slamDunks : 1, 
+}
+
+const player6 = {
+    playerName: "Jeff Adrien",
+    Number : 4,
+    Shoe : 18,
+    Points : 10,
+    Rebounds : 1,
+    Assists : 1,
+    Steals : 2,
+    Blocks : 7,
+    slamDunks : 2, 
+}
+
+const player7 = {
+    playerName: "Bismak Biyombo",
+    Number : 0,
+    Shoe : 16,
+    Points : 12,
+    Rebounds : 4,
+    Assists : 7,
+    Steals : 7,
+    Blocks : 15,
+    slamDunks : 10, 
+}
+
+const player8 = {
+    playerName: "DeSagna Diop",
+    Number : 2,
+    Shoe : 14,
+    Points : 24,
+    Rebounds : 12,
+    Assists : 12,
+    Steals : 4,
+    Blocks : 5,
+    slamDunks : 5, 
+}
+
+const player9 = {
+    playerName: "Ben Gordon",
+    Number : 8,
+    Shoe : 15,
+    Points : 33,
+    Rebounds : 3,
+    Assists : 2,
+    Steals : 1,
+    Blocks : 1,
+    slamDunks : 0, 
+}
+
+const player("Brendan Haywood") {
+    // playerName: "Brendan Haywood",
+    Number : 33,
+    Shoe : 15,
+    Points : 6,
+    Rebounds : 12,
+    Assists : 12,
+    Steals : 22,
+    Blocks : 5,
+    slamDunks : 12, 
+}
+
+
+gameObject.away = {}
+gameObject.away.teamName = "Brooklyn Nets"
+gameObject.away.colors = ['Black', 'White']
+gameObject.away.players = {player6, player7, player8, player9, player10}
+
+gameObject.home.teamname = "Brooklyn Nets"
+gameObject.home.colors = []
+gameObject.home.players = {player1, player2, player3, player4, player5}
+
+
+
+
+
+
+//console.log(gameObject.home.players.player1.Shoe)
+console.log(gameObject)
+
